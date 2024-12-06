4febc0432fcbf94f53db6e4d82da47dc0f78ca9e
Yash Pawar <pawaryash6262@gmail.com>
2024-12-06 10:52:36 +0530

    Add files via upload

---
+++ Yash-Portfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config={
+    theme:{
+        fontFamily:{
+            Outfit:["Outfit", "sans-serif"],
+            Ovo:["Ovo", "serif"]
+
+
+        },
+        animation:{
+            spin_slow:'spin 6s linear infinite'
+        }
+    },
+
+    darkmode:'selector'
+} 
